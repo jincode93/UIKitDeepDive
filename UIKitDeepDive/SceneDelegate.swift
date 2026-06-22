2bb16e0bff68e07626b0b523edb1701a27d40eda
jincode93 <jincode93@gmail.com>
2026-06-22 16:01:37 +0900

    feat: 프로젝트 초기 설정

---
+++ UIKitDeepDive/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  UIKitDeepDive
+//
+//  Created by 진준호 on 6/22/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
---
+++ UIKitDeepDive/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIKitDeepDive
+//
+//  Created by 진준호 on 6/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
