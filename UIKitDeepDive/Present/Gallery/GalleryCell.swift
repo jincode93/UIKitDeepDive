8f5fa481f4241f962c6ae4f4e3360995e861f8db
jincode93 <jincode93@gmail.com>
2026-06-23 10:47:59 +0900

    feat: Gallery Collection View 구현

---
+++ UIKitDeepDive/Present/Gallery/GalleryCell.swift
@@ -0,0 +1,8 @@
+//
+//  GalleryCell.swift
+//  UIKitDeepDive
+//
+//  Created by 진준호 on 6/23/26.
+//
+
+import Foundation
---
+++ UIKitDeepDive/Present/Gallery/GalleryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryViewController.swift
+//  UIKitDeepDive
+//
+//  Created by 진준호 on 6/22/26.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
